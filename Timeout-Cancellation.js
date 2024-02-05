9161a4a37e66c810498da147e57065de7afa13eb
Hammyboi09 <97976408+Hammyboi09@users.noreply.github.com>
2024-02-05 20:13:40 +0530

    Add files via upload

---
+++ Timeout-Cancellation.js
@@ -0,0 +1,14 @@
+var cancellable = function(fn, args, t) {
+    // call setTimeout, which is set to call fn after t amount of time
+    var timeout = setTimeout(() =>
+        fn(...args)
+    , t)
+
+    // define a clearTimeout
+    var cancelFn = () => clearTimeout(timeout);
+
+    // When/if we call the function, it will return cancelFn,
+    // and since the return line calls (and consequentially executes)
+    // cancelFn, timeout will be cancelled, thereby cancelling fn
+    return cancelFn;
+};
